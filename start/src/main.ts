f03aac81a5f32c69d7f09e916b9bb72954ac4a90
jycr <jycr@users.noreply.github.com>
2024-04-18 14:14:39 +0200

    partie 2 - convertion en standalone
    ```
    ng g @angular/core:standalone
    ```
    
    ❯ Bootstrap the application using standalone APIs

--- start/src/main.ts
+++ start/src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, MatButtonModule, MatToolbarModule, MatBadgeModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
